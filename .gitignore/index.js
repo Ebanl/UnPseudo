2a84a1d51d15a89e95a91c46b7b2da205c2dd95e
Ebanl <35999894+Ebanl@users.noreply.github.com>
2018-03-27 14:09:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,38 +11,4 @@ bot.login(process.env.TOKEN);
 
 bot.on("message", message => {
     if (message.content === "modo")
-        message.reply(" c'est mon chef");})
-bot.on("message", message => {
-    if (message.content === "salut")
-        message.reply(" bonjour");})
-bot.on("message", message => {
-    if (message.content === "Modo") 
-        message.reply(" c'est mon chef");})
-bot.on("message", message => {
-    if (message.content === "Salut") 
-        message.reply(" bonjour");}) 
-bot.on("message", message => {
-    if (message.content === "Hihax") 
-        message.reply(" c'est mon dev");}) 
-bot.on("message", message => {
-    if (message.content === "Jerey")
-        message.reply(" c'est le fondateur du serveur !!!")
-    if (message.content === "qui est la") 
-        message.reply("Moi je suis la comme ça t'es jamais Rémi c'est cool non ?? ") 
-    if (message.content === "Le français")
-        message.reply("c'est de la merde") ;}) 
-bot.on("message", message => {
-    if (message.content === "le francais") 
-        message.reply(" c'est de la merde");})
-bot.on("message", message => {
-    if (message.content === "jerey") 
-        message.reply(" c'est le fondateur du serveur !!! ");})
-bot.on("message", message => {
-    if (message.content === "hihax") 
-        message.reply(" c'est mon dev");}) 
-bot.on("message", message => {
-    if(message.content === "Bonjour"){
-    const broadcast = client.createVoiceBroadcast(); 
-    broadcast.playFile('UnPseudo/.gitignore/33626998.mp3');
-    const dispatcher = connection.playBroadcast(broadcast); }
-
+        message.reply(" modo");})
